9b16ee2aa9020c3b3b2c708c006e6e088094f6ad
K S HARIHARA PRIYAN <itsmehhpriyan@gmail.com>
2026-02-27 20:10:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2313482457-d0b7e",
+  "appId": "1:599371130127:web:cf1829fc13795dabe48a7a",
+  "apiKey": "AIzaSyBzr9gw5xxxdAyiaEuFvvNxG-ADnmdyNXw",
+  "authDomain": "studio-2313482457-d0b7e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "599371130127"
+};
